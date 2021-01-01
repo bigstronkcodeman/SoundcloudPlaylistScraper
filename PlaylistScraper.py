3e710807cf43e58f8e8cb5afdcecf6df34e55bde
Phillip Boudreau <43645849+bigstronkcodeman@users.noreply.github.com>
2020-12-31 23:05:58 -0600

    Update PlaylistScraper.py

--- PlaylistScraper.py
+++ PlaylistScraper.py
@@ -3,11 +3,14 @@ import tkinter as tk
 import tkinter.messagebox
 import tkinter.filedialog
 import requests as rq
+import shutil
 import re
 import json
 import threading
 from PIL import Image, ImageTk
 from functools import partial
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, TPE1, error
 
 CLIENT_ID = "2412b70da476791567d496f0f3c26b88"
 APPENDUM = "?client_id=" + CLIENT_ID
@@ -46,16 +49,31 @@ def request_api_data_json(url):
     r = rq.get(url)
     return json.loads(r.text)
 
-def download_api_mp3(url, path):
-    r = rq.get(url, stream=True)
+def download_api_mp3(audio_url, art_url, artist, path):
+    r = rq.get(audio_url, stream=True)
     with open(path, "wb") as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
+    mp3 = MP3(path, ID3=ID3)
+    try:
+        mp3.add_tags()
+    except error:
+        pass
+    if art_url is not None:
+        img_response = rq.get(art_url, stream=True)
+        with open("temp.jpg", "wb") as f:
+            shutil.copyfileobj(img_response.raw, f)
+        del img_response
+        mp3.tags.add(APIC(mime="image/jpeg", type=3, desc=u"Cover", data=open("temp.jpg", "rb").read()))
+    mp3.tags.add(TPE1(encoding=3, text=f"{artist}"))
+    mp3.save()
     return
 
 def playlist_download_thread(folder_path, url_path, permalink):
-    playlists_url = "https://api.soundcloud.com/users/{}/playlists.json{}".format(permalink, APPENDUM)
+    profile_url = f"https://api.soundcloud.com/users/{permalink}{APPENDUM}"
+    profile_id = request_api_data_json(profile_url)["id"]
+    playlists_url = f"https://api.soundcloud.com/users/{profile_id}/playlists.json{APPENDUM}"
     playlist_json_list = request_api_data_json(playlists_url)
     for playlist in playlist_json_list:
         playlist_permalink = playlist["permalink"]
@@ -64,8 +82,11 @@ def playlist_download_thread(folder_path, url_path, permalink):
             if not os.path.isdir(track_path):
                 os.makedirs(track_path)
             if track["streamable"]:
-                print("Downloading " + track["title"] + " by " + track["user"]["username"] + " in playlist " + playlist["title"] + "...")
-                download_api_mp3(track["stream_url"] + APPENDUM, track_path + "/" + track["permalink"] + ".mp3")
+                print(f"Downloading {track['title']} by {track['user']['username']} in playlist {playlist['title']}...")
+                track_name = track["title"]
+                for c in [ "<", ">", ":", "\"", "/", "\\", "|", "?", "*" ]:
+                    track_name = track_name.replace(c, " ")
+                download_api_mp3(f"{track['stream_url']}{APPENDUM}", track["artwork_url"], track["user"]["username"], f"{track_path}/{track_name}.mp3")
                 print("done.")
     print("All playlists downloaded.")
     return
