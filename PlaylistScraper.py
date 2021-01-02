f4a1aa867ffb607fcb384feb300de0353a350ba4
Phillip Boudreau <43645849+bigstronkcodeman@users.noreply.github.com>
2021-01-01 20:17:04 -0600

    Update PlaylistScraper.py

--- PlaylistScraper.py
+++ PlaylistScraper.py
@@ -65,9 +65,7 @@ def download_api_mp3(audio_url, art_url, artist, path):
     except error:
         pass
     if art_url is not None:
-        to_repl = "large"
-        repl = "t500x500";
-        art_url = repl.join(art_url.rsplit(to_repl, 1)) # get high resolution artwork instead of the one sc wants you to have
+        art_url = "t500x500".join(art_url.rsplit("large", 1)) # get high resolution artwork instead of the shitty low res one sc wants you to have
         img_response = rq.get(art_url, stream=True)
         with open("temp.jpg", "wb") as f:
             shutil.copyfileobj(img_response.raw, f)
